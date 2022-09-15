58fe9e4ff09eef8c9c7cc533a1236c4167f81028
liux0275 <84728523+liux0275@users.noreply.github.com>
2022-09-15 17:21:43 -0400

    Create database.php

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+function conDB() {
+  $servername = "localhost";
+  $username = "server";
+  $password = "Neodb13579!";
+  $dbname = "cp476";
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
+  return $conn;
+}
+?>
